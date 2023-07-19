dbb63dcc0d054f069ad954f1c2e14a8015d69c2e
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2023-07-19 22:10:04 +0200

    add groups permissions for django admin site

--- user/models.py
+++ user/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import Group
-from user.managers import CustomUserManager
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-from staff.models import Employee
+from user.managers import CustomUserManager
 
 
 class User(AbstractUser):
@@ -16,18 +14,44 @@ class User(AbstractUser):
 
     objects = CustomUserManager()
 
-
     def __str__(self):
         return str(self.id) + " - " + str(self.email)
 
-    # managers = Group.objects.get_or_create(name='managers')
-    # sales = Group.objects.get_or_create(name='sales')
-    # support = Group.objects.get_or_create(name='support')
+
+management_team, created_a = Group.objects.get_or_create(name='Management-Team')
+sales_team, created_b = Group.objects.get_or_create(name='Sales-Team')
+support_team, created_c = Group.objects.get_or_create(name='Support-Team')
 
 
-    # managers.permissions.set([permission_list])
+def group_permissions(group, crud_operations, model_list):
+    for model in model_list:
+        for operation in crud_operations:
+            permission = Permission.objects.get(name='Can' + " " + operation + " " + model)
+            group.permissions.add(permission)
 
-    # def save(self, *args, **kwargs):
-    #     managers = Group.objects.get(name="managers")
-    #     self.groups.add(managers)
-    #     super().save(*args, **kwargs)
+
+all_operations = ['add', 'change', 'delete', 'view']
+change_and_view = ['change', 'view']
+model_list_1 = ['user', 'employee']
+model_list_2 = ['team', 'customer', 'contract', 'event']
+group_permissions(management_team, all_operations, model_list_1)
+group_permissions(management_team, change_and_view, model_list_2)
+
+
+add_change_view = ['add', 'change', 'view']
+model_list_3 = ['customer', 'contract', 'event']
+group_permissions(sales_team, add_change_view, model_list_3)
+
+model_list_4 = ['customer', 'event']
+group_permissions(support_team, change_and_view, model_list_4)
+
+
+# employee_in_management_team = [Employee.objects.filter(team__department='Management')]
+# user_in_management_team = [employee.user for employee in employee_in_management_team]
+#
+# employee_in_sales_team = [Employee.objects.filter(team__department='Sales')]
+# user_in_sales_team = [employee.user for employee in employee_in_sales_team]
+#
+# for user in User.objects.all():
+#     if user in user_in_sales_team:
+#         sales_team.user_set.add(user)
